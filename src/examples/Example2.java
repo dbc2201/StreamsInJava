7a61a553bb65d6032231eaf9b0ccf225a1bc6277
divyansh <divyanshb22@gmail.com>
2020-12-01 23:53:26 +0530

    add comments for the method reference

--- src/examples/Example2.java
+++ src/examples/Example2.java
@@ -5,6 +5,9 @@ import java.util.stream.IntStream;
 public class Example2 {
     public static void main(String[] args) {
         var range = IntStream.range(1, 100);
-        range.forEach(System.out::println);
+        // Method Reference
+        // ClassName(.ObjectName[OPTIONAL])::methodName
+        // System    .out                  ::print
+        range.forEach(System.out::print);
     }
 }
