273eda2d26fa44509cba4f3b638aa6a60dafb4d5
divyansh <divyanshb22@gmail.com>
2020-12-02 00:10:55 +0530

    create an arraylist of integers

--- src/examples/Example4.java
+++ src/examples/Example4.java
@@ -1,7 +1,9 @@
 package examples;
 
+import java.util.ArrayList;
+
 public class Example4 {
     public static void main(String[] args) {
-
+        ArrayList<Integer> list = new ArrayList<>();
     }
 }
