83b5e497b75cd5f96883f4d517527d8ea979a399
divyansh <divyanshb22@gmail.com>
2020-12-02 00:03:16 +0530

    pass a lambda expression to the forEach() method

--- src/examples/Example3.java
+++ src/examples/Example3.java
@@ -5,5 +5,6 @@ import java.util.stream.IntStream;
 public class Example3 {
     public static void main(String[] args) {
         var range = IntStream.range(1, 100);
+        range.forEach(number -> System.out.println(number + " "));
     }
 }
