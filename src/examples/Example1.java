d056089349fae0e6b3379bbebf588733812496df
divyansh <divyanshb22@gmail.com>
2020-12-01 09:30:06 +0530

    add some random numbers to the arraylist

--- src/examples/Example1.java
+++ src/examples/Example1.java
@@ -6,5 +6,11 @@ public class Example1 {
     public static void main(String[] args) {
         // An ArrayList of Integer numbers.
         ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(7);
+        list.add(18);
+        list.add(24);
+        list.add(17);
+        list.add(5);
     }
 }
