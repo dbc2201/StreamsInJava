7563c058a1961cb8a996aa14d43981ed240e8200
divyansh <divyanshb22@gmail.com>
2020-12-02 00:12:02 +0530

    stream the arraylist

--- src/examples/Example4.java
+++ src/examples/Example4.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 
 public class Example4 {
     public static void main(String[] args) {
+        // Data Source
         ArrayList<Integer> list = new ArrayList<>();
+
+        // Let us "stream" it!
+        var stream = list.stream(); // Stream<Integer>
     }
 }
