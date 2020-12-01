370ec61a0021dca55c3f5b37521ddaf1431ab75f
divyansh <divyanshb22@gmail.com>
2020-12-01 09:29:22 +0530

    create an arraylist of integer type

--- src/examples/Example1.java
+++ src/examples/Example1.java
@@ -1,7 +1,10 @@
 package examples;
 
+import java.util.ArrayList;
+
 public class Example1 {
     public static void main(String[] args) {
-
+        // An ArrayList of Integer numbers.
+        ArrayList<Integer> list = new ArrayList<>();
     }
 }
