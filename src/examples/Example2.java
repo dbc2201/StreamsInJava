2ed1c447123d53128f85f50b8cd3163a088446d3
divyansh <divyanshb22@gmail.com>
2020-12-01 23:51:52 +0530

    create a stream of integer numbers from 1 to 99

--- src/examples/Example2.java
+++ src/examples/Example2.java
@@ -1,7 +1,9 @@
 package examples;
 
+import java.util.stream.IntStream;
+
 public class Example2 {
     public static void main(String[] args) {
-
+        var range = IntStream.range(1, 100);
     }
 }
